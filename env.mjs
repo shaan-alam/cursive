5bae74bb384f31784c61fee69329c03048f4bb2c
shaan-m0 <shaan@m0.ventures>
2024-01-28 02:27:17 +0530

    initial setup

---
+++ env.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { z } from "zod";
+
+/**
+ * Specify your environment variables schema here.
+ * This way, you can ensure the app isn't built with invalid environment variables.
+ * By default, environment variables are only available in the Node.js environment, meaning they won't be exposed to the browser. In order to expose a variable to the browser you have to prefix the variable with NEXT_PUBLIC_.
+ * -> Don't use any Zod .transform() methods in the schema (will cause `implicitly has type 'any'` error) <-
+ */
+export const schema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  NAME: z.string()
+});
+
+/**
+ * Environment variable declarations based on the schema help structure your environment variables programmatically.
+ * @type {{ [k in keyof z.infer<typeof schema>]: z.infer<typeof schema>[k] | undefined }}
+ */
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+  NAME: process.env.NAME
+};
+
+/**
+ * --------------------------------
+ * --------------------------------
+ * Next-ValidEnv Manual Implementation
+ * --------------------------------
+ * --------------------------------
+ */
+
+export const formatZodErrors = (
+  /** @type z.ZodFormattedError<Map<string, string>, string> */ errors
+) =>
+  Object.entries(errors)
+    .map(([name, value]) => {
+      if (value && "_errors" in value)
+        return `${name}: ${value._errors.join(", ")}\n`;
+
+      return;
+    })
+    .filter(Boolean);
+
+const safeParsedEnv = schema.safeParse(env);
+
+if (!safeParsedEnv.success) {
+  console.error(
+    "❌ Invalid environment variables:\n",
+    ...formatZodErrors(safeParsedEnv.error.format())
+  );
+  throw new Error("Invalid environment variables");
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./env.mjs"));
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+};
+export default config;
